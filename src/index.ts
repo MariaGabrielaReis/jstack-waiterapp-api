1cbc1b5b553457476ef04389068d2ba8e6e659a8
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-10 20:34:00 -0300

    feat: create connection to database (mongodb)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
+mongoose
+  .connect("mongodb://localhost:27017")
+  .then(() => {
+    const app = express();
+    const PORT = 3333;
 
-const PORT = 3333;
-app.listen(PORT, () =>
-  console.log(`🔥 Server is running on http://localhost:${PORT}`)
-);
+    app.listen(PORT, () =>
+      console.log(`🔥 Server is running on http://localhost:${PORT}`)
+    );
+  })
+  .catch(() => console.log("Error to connect MongoDB"));
