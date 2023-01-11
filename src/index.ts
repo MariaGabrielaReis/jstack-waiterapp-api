00c0d4431495f7463c09cd30de071c9f78a01529
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-10 21:10:50 -0300

    feat: create use case to list categories

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 
+import { router } from "./router";
+
 mongoose
   .connect("mongodb://localhost:27017")
   .then(() => {
     const app = express();
     const PORT = 3333;
+
+    app.use(router);
 
     app.listen(PORT, () =>
       console.log(`🔥 Server is running on http://localhost:${PORT}`)
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
+import { listCategories } from "./app/use-cases/categories/list-categories";
 
 export const router = Router();
 
 // List categories
-router.get("/categories", (req, res) => {
-  res.send("OK");
-});
+router.get("/categories", listCategories);
 
 // Create category
 router.post("/categories", (req, res) => {
