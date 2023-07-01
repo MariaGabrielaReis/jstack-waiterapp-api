8e9868757e30f707e0f3af3726dc3e19132375a2
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-07-01 09:09:14 -0300

    feat: use websockets to send new order in real time

--- src/app/use-cases/orders/create-order.ts
+++ src/app/use-cases/orders/create-order.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 
+import { io } from "../../..";
 import { Order } from "../../models/Order";
 
 export async function createOrder(req: Request, res: Response) {
@@ -14,6 +15,9 @@ export async function createOrder(req: Request, res: Response) {
       table,
       products,
     });
+
+    const orderDetails = await order.populate("products.product");
+    io.emit("orders@new", orderDetails);
 
     res.status(201).json(order);
   } catch (error) {
